57b1e8ae3e498538075db8bd6f228045a1665373
BjornKlarstrom <bjorn.klarstrom@gmail.com>
2020-10-20 18:12:19 +0200

    Added GoldScript with Text Update

--- Assets/Scripts/Gold.cs
+++ Assets/Scripts/Gold.cs
@@ -1,8 +1,17 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class Gold : MonoBehaviour
 {
-    
+    public int goldAmount = 0;
+    public Text goldAmountText;
+
+    private void Update()
+    {
+        this.goldAmountText.text = this.goldAmount.ToString("Gold: 0");
+    }
 }
