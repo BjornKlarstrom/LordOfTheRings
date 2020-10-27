4eb94d05fc5ff26979eab74a880e27d8d50c6b36
BjornKlarstrom <bjorn.klarstrom@gmail.com>
2020-10-27 21:25:13 +0100

    adds new scene and creates a character class and hero class

---
+++ Assets/Scripts/Hero/Character.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    public int health = 100;
+    public float chargingAttackTime = 1.0f;
+    public float attackDamage = 5.0f;
+
+    public void Attack()
+    {
+        Debug.Log("Hej");
+    }
+}
---
+++ Assets/Scripts/Hero/Hero.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class Hero : Character
+{
+    chargingAttackTime
+    private void Start()
+    {
+        Attack();
+        
+    }
+}
